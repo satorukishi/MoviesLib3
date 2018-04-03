b8cf4c51b2266fa0a25e89bb32b5b828752bd440
Usuário Convidado <Guest@pa06micro01.local>
2018-04-02 23:01:43 -0300

    Utilizando MapView e SearchBar

--- MoviesLib3/TheatersMapViewController.swift
+++ MoviesLib3/TheatersMapViewController.swift
@@ -18,13 +18,17 @@ class TheatersMapViewController: UIViewController {
     var currentElement: String!
     var theater: Theater!
     var theaters: [Theater] = []
+    lazy var locationManager = CLLocationManager() // lazy significa que a variável só será instanciada se utilizada
+    var poiAnnotations: [MKPointAnnotation] = []
     
     // MARK: - Super Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        mapView.delegate = self
         loadXML()
+        
+        requestUserLocationAuthorization()
     }
     
     // MARK: - Methods
@@ -43,6 +47,27 @@ class TheatersMapViewController: UIViewController {
             mapView.addAnnotation(annotation)
         }
         
+        mapView.showAnnotations(mapView.annotations, animated: true)
+    }
+    
+    func requestUserLocationAuthorization() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+//            locationManager.allowsBackgroundLocationUpdates = true
+            locationManager.pausesLocationUpdatesAutomatically = true
+            
+            switch CLLocationManager.authorizationStatus() {
+            case .authorizedAlways, .authorizedWhenInUse:
+                print("Usuário já autorizou o uso da localização")
+            case .denied:
+                print("Usuário negou a autorização")
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()
+            case .restricted:
+                print("O usuário não está autorizado a habilitar a localização")
+            }
+        }
     }
 }
 
@@ -98,8 +123,63 @@ extension TheatersMapViewController: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         var annotationView: MKAnnotationView!
         
-        
+        if annotation is TheaterAnnotation {
+            annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "Theater")
+            
+            if annotationView == nil {
+                annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "Theater")
+                annotationView.image = UIImage(named: "theatherIcon")
+                annotationView.canShowCallout = true // vai mostrar o balaozinho
+            } else { // significa que é uma annotationView que já foi carregada e utilizada em outro ponto
+                annotationView.annotation = annotation
+            }
+        }
         
         return annotationView
+    }
+}
+
+// MARK: - CLLocationManagerDelegate
+extension TheatersMapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+        default:
+            break
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        print("Velocidade do Usuário \(userLocation.location?.speed ?? 0)")
+        
+//        let region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 500, 500)
+////        MKCoordinateRegion(center: CLLocationCoordinate2D, span: MKCoordinateSpan)
+//        mapView.setRegion(region, animated: true)
+    }
+}
+
+// MARK: - UISearchBarDelegate
+extension TheatersMapViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = searchBar.text!
+        request.region = mapView.region // a pesquisa será feita na região que está mostrando na tela
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            if error == nil {
+                guard let response = response else {return}
+                self.mapView.removeAnnotations(self.poiAnnotations)
+                self.poiAnnotations.removeAll()
+                for item in response.mapItems {
+                    let place = MKPointAnnotation()
+                    place.coordinate = item.placemark.coordinate
+                    place.title = item.name
+                    place.subtitle = item.phoneNumber
+                    self.poiAnnotations.append(place)
+                }
+                
+            }
+        }
     }
 }
